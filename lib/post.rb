d4df581775fcfd1caff6f35165383edc941031ad
Jackson Reynolds <jwreynold@gmail.com>
2020-07-20 15:55:29 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,6 +2,20 @@ require 'author.rb'
 
 class Post
 
+  attr_accessor :title, :author
 
+  @@all = []
+
+  def initialize(title)
+    @title = title
+  end #initialize
+
+  def self.all
+    @@all
+  end #self.all
+
+  def author_name
+    self.author.name if self.author
+  end #author_name
 
 end #Post
